87ff5f582378db7b7c5250b303a953cbdace2f84
K-action123 <hoodkevin49@gmail.com>
2025-08-31 00:00:24 +0200

    added untrusted things

--- accounts-ms/backend/server.js
+++ accounts-ms/backend/server.js
@@ -1,1 +1,32 @@
-console.log("Hello World!");
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// Import The User routes
+const userRoutes  = require('./src/routes/userRoutes');
+
+// loading environment variable
+dotenv.config();
+const app=express();
+const PORT = process.env.PORT || 5000;
+
+// connecting to mongoDB
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MONGODB connected Successfully!");
+    }catch(error){
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+connectDB();
+
+// MIDDLEWARE to parse incoming json Requests
+
+app.use(express.json());
+app.use('/api/auth',userRoutes); // connecting routes to the server
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on Port ${PORT}`);
+});
---
+++ accounts-ms/backend/src/controllers/userController.js
@@ -0,0 +1,37 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/User');
+
+const registerUser= asyncHandler(async(req, res)=>{
+    const {firstName, lastName, email, telephone, password} = req.body;
+
+    // Simple Validation to ensure all fields are present
+    if(!firstName || !lastName || !password || (!email && !telephone)){
+        res.status(400);
+        throw new Error('Please fill all required fields');
+    }
+
+    // Checking if a user with the same email or telephone exists
+    const userExists = await User.findOne({
+        $or: [{ email: email }, { telephone: telephone }]
+    });
+
+    if (userExists){
+        res.status(400);
+        throw new Error(" User already exists with this Email or telephone number");
+    }
+    // Create the new User 
+    const user = await User.findOne({
+        firstName,
+        lastName,
+        email,
+        telephone,
+        password,
+    });
+    if (user){
+        res.status(201).json({message:"User registered Successfully!"});
+    }else{
+        res.status(400);
+        throw new Error('Invalid user Data');
+    }
+});
+module.exports = { registerUser };
